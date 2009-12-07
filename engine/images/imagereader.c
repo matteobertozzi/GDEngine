73bc923b628ad465b8ab7319edb0306ba8f172c5
Matteo Bertozzi <theo.bertozzi@gmail.com>
2009-12-07 20:16:28 +0100

    Added license header

--- engine/images/imagereader.c
+++ engine/images/imagereader.c
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2009, Matteo Bertozzi <theo.bertozzi@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Matteo Bertozzi nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Matteo Bertozzi ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Matteo Bertozzi BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <GDEngine/Config.h>
 
 #include "imagereader.h"
@@ -54,12 +81,6 @@ GDImage *gdImageReadFromFile (GDImage *image, const char *filename) {
     GDUInt i, n;
     FILE *fh;
     
-    char buffer[4096];
-        snprintf(buffer, 4095,
-            "/Users/oz/Desktop/GDEngine/TombRaider/DATA/%s",
-            strrchr(filename, '/'));
-    filename = buffer;
-
     /* First, try to lookup Image in Pool */
     if ((poolImg = gdImagePoolFind(filename)) != NULL)
         return(image ? gdImageCopy(image, poolImg) : gdImageRetain(poolImg));
